d720b13153d4196ae2e0c00e0a43a2404df2eb4c
Radoslav Sharapanov <spacest@gmail.com>
2021-07-09 22:36:59 +0300

    ,

--- n-carousel.js
+++ n-carousel.js
@@ -117,6 +117,41 @@
 
   const isAuto = (el) => el.parentNode.matches(".n-carousel--auto-height");
 
+  const trapFocus = (modal) => {
+    // FROM: https://uxdesign.cc/how-to-trap-focus-inside-modal-to-make-it-ada-compliant-6a50f9a70700
+    // add all the elements inside modal which you want to make focusable
+    const focusableElements = 'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
+
+    const firstFocusableElement = modal.querySelectorAll(focusableElements)[0]; // get first element to be focused inside modal
+    const focusableContent = modal.querySelectorAll(focusableElements);
+    const lastFocusableElement = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
+
+    document.addEventListener("keydown", function (e) {
+      let isTabPressed = e.key === "Tab" || e.keyCode === 9;
+
+      if (!isTabPressed) {
+        return;
+      }
+
+      if (e.shiftKey) {
+        // if shift key pressed for shift + tab combination
+        if (document.activeElement === firstFocusableElement) {
+          lastFocusableElement.focus(); // add focus for the last focusable element
+          e.preventDefault();
+        }
+      } else {
+        // if tab key is pressed
+        if (document.activeElement === lastFocusableElement) {
+          // if focused has reached to last focusable element then focus first focusable element after pressing tab
+          firstFocusableElement.focus(); // add focus for the first focusable element
+          e.preventDefault();
+        }
+      }
+    });
+
+    firstFocusableElement.focus();
+  };
+
   // Fix snapping with mouse wheel. Thanks https://stackoverflow.com/a/62415754/3278539
 
   //   const detectTrackPad = (e) => {
@@ -304,6 +339,14 @@
 
     el.dataset.x = Math.abs(Math.round(scrollStartX(el) / ceilingWidth(el.children[0])));
     el.dataset.y = Math.abs(Math.round(el.scrollTop / ceilingHeight(el.children[0])));
+
+    // When inline
+    if (el.dataset.x === "NaN") {
+      el.dataset.x = 0;
+    }
+    if (el.dataset.y === "NaN") {
+      el.dataset.y = 0;
+    }
 
     let active = getIndex(el);
 
@@ -583,6 +626,7 @@
       const carousel = el.closest(".n-carousel");
       if (carousel.classList.contains("n-carousel--inline") && !carousel.classList.contains("n-carousel--overlay")) {
         carousel.classList.add("n-carousel--overlay");
+        trapFocus(carousel);
       }
       slideTo(closestCarousel(el), [...el.parentNode.children].indexOf(el));
     }
