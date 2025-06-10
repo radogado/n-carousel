c45e7c7e7d5d037b69f9bd22ea9a632c7f5232a2
Radoslav Sharapanov <rado@me.com>
2025-06-10 17:13:44 +0300

    ,

--- build.sh
+++ build.sh
@@ -1,17 +1,23 @@
+#!/bin/bash
+
+# Compile SASS
 ./node_modules/sass/sass.js n-carousel.scss n-carousel.css
 ./node_modules/sass/sass.js demo/demo.scss demo/demo.css
+
+# Minify CSS
 ./node_modules/clean-css-cli/bin/cleancss -o n-carousel.min.css n-carousel.css
 
-# Use Rollup to bundle n-carousel.js and scrollyfills.module.js into a single file
-./node_modules/rollup/dist/bin/rollup n-carousel.js --format es --file n-carousel.rollup.js
-
-# Minify the bundled file
+# Bundle and minify JavaScript using Rollup config
+./node_modules/rollup/dist/bin/rollup -c rollup.config.js
 ./node_modules/terser/bin/terser -o n-carousel.min.js --source-map "url='n-carousel.min.js.map'" --compress --mangle -- n-carousel.rollup.js
 
-# Clean up temporary files
-rm n-carousel.rollup.js
-
+# Minify preload script
 ./node_modules/terser/bin/terser -o n-carousel-preload.min.js --source-map "url='n-carousel-preload.min.js.map'" --compress --mangle -- n-carousel-preload.js
+
+# Calculate gzip sizes
 ./node_modules/gzip-size-cli/cli.js --raw n-carousel.min.css > n-carousel.min.css.size
 ./node_modules/gzip-size-cli/cli.js --raw n-carousel.min.js > n-carousel.min.js.size
 ./node_modules/gzip-size-cli/cli.js --raw n-carousel-preload.min.js > n-carousel-preload.min.js.size
+
+# Clean up temporary files
+rm n-carousel.rollup.js
--- n-carousel.rollup.js
+++ n-carousel.rollup.js
@@ -1,5 +1,6 @@
+function e(e,t){(null==t||t>e.length)&&(t=e.length);for(var n=0,r=new Array(t);n<t;n++)r[n]=e[n];return r}function t(t,n){var r="undefined"!=typeof Symbol&&t[Symbol.iterator]||t["@@iterator"];if(r)return (r=r.call(t)).next.bind(r);if(Array.isArray(t)||(r=function(t,n){if(t){if("string"==typeof t)return e(t,n);var r=Object.prototype.toString.call(t).slice(8,-1);return "Object"===r&&t.constructor&&(r=t.constructor.name),"Map"===r||"Set"===r?Array.from(t):"Arguments"===r||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(r)?e(t,n):void 0}}(t))||n&&t&&"number"==typeof t.length){r&&(t=r);var o=0;return function(){return o>=t.length?{done:!0}:{done:!1,value:t[o++]}}}throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}if(!("onscrollend"in window)){var n=function(e,t,n){var r=e[t];e[t]=function(){var e=Array.prototype.slice.apply(arguments,[0]);r.apply(this,e),e.unshift(r),n.apply(this,e);};},r=function(e,t,n,r){if("scroll"==t||"scrollend"==t){var o=this,s=l.get(o);if(void 0===s){var c=0;s={scrollListener:function(e){clearTimeout(c),c=setTimeout(function(){a.size?setTimeout(s.scrollListener,100):(o.dispatchEvent(i),c=0);},100);},listeners:0},e.apply(o,["scroll",s.scrollListener]),l.set(o,s);}s.listeners++;}},o=function(e,t,n){if("scroll"==t||"scrollend"==t){var r=this,o=l.get(r);void 0!==o&&(o[t]--,--o.listeners>0||(e.apply(r,["scroll",o.scrollListener]),l.delete(r)));}},i=new Event("scrollend"),a=new Set;document.addEventListener("touchstart",function(e){for(var n,r=t(e.changedTouches);!(n=r()).done;)a.add(n.value.identifier);},{passive:!0}),document.addEventListener("touchend",function(e){for(var n,r=t(e.changedTouches);!(n=r()).done;)a.delete(n.value.identifier);},{passive:!0});var l=new WeakMap;n(Element.prototype,"addEventListener",r),n(window,"addEventListener",r),n(document,"addEventListener",r),n(Element.prototype,"removeEventListener",o),n(window,"removeEventListener",o),n(document,"removeEventListener",o);}
+
 // import './node_modules/n-modal/n-modal.js';
-import "./scrollyfills.module.js"; // scrollend event polyfill
 
 // Define translations and initWithTranslations before the main carousel code
 let translations = {};
@@ -17,7 +18,6 @@ window.initWithTranslations = function(customTranslations) {
 };
 
 (function () {
-  "use strict";
 
   const ceilingWidth = (el) =>
     Math.ceil(parseFloat(getComputedStyle(el).width));
@@ -91,11 +91,10 @@ window.initWithTranslations = function(customTranslations) {
     }
     // console.log(index);
     carousel.parentNode.dataset.sliding = true;
-    let timeout = 0;
     delete carousel.dataset.next;
     observersOff(carousel);
-    let x = carousel.scrollLeft;
-    let y = carousel.scrollTop;
+    carousel.scrollLeft;
+    carousel.scrollTop;
     let interval = 10; // Get rid of this magic number by timeout comparison with previous scroll offset
     let timeout_function = () => {
       // console.log(entry, entry.target, 'is intersecting at', entry.target.parentElement.scrollLeft, entry.target.parentElement.scrollTop);
@@ -156,7 +155,7 @@ window.initWithTranslations = function(customTranslations) {
         });
       }
     };
-    timeout = setTimeout(timeout_function, interval);
+    setTimeout(timeout_function, interval);
   };
   const hashNavigation = (e) => {
     // Hash navigation support
@@ -455,10 +454,8 @@ window.initWithTranslations = function(customTranslations) {
       var duration =
         parseFloat(el.parentNode.dataset.duration) * 1000 || default_duration;
       var start = null;
-      var end = null;
       let startAnim = (timeStamp) => {
         start = timeStamp;
-        end = start + duration;
         draw(timeStamp);
       };
       let draw = (now) => {
@@ -1444,4 +1441,4 @@ window.initWithTranslations = function(customTranslations) {
     document.addEventListener("DOMContentLoaded", doInit);
   }
 })();
-
+//# sourceMappingURL=n-carousel.rollup.js.map
